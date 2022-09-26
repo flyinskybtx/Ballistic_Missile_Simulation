9aae4bfc26db389cbbb2f277a043dafdfb735aa3
flyinsky <flyinskybtx@sina.com>
2022-09-26 18:14:33 +0800

    可以运行，修改了部分变量名

--- Ballistic_trajectory.py
+++ Ballistic_trajectory.py
@@ -16,154 +16,188 @@ from matplotlib.gridspec import GridSpec
 import os
 
 # Fundamental Constants
-G = 6.67e-11     # Gravitational Constant
-M_E = 5.972e24   # kg
-R_E = 6371000    # m
-g   = 9.81       # m/s^2
-omega = 7.2921159e-5 # rad/s
+GRAV_CONST = 6.67e-11  # Gravitational Constant
+MASS_EARTH = 5.972e24  # kg
+RADIUS_EARTH = 6371000  # m
+g_const = 9.81  # m/s^2
+OMEGA_EARTH = 7.2921159e-5  # rad/s
 
 # Missile
-m_0 = 6200 # kg, launch mass
-m_f = 1600 # kg, mass of war head + empty rocket 
-m_wh = 800 # kg, mass of war head
-isp = 237 # s, SRB general number
-v_e = isp*g # m/s
-TTW = 1.5 # thrust to weight
-F = m_0*g*TTW # thrust, in N
-C_D = 2e-1 # drag coefficient, dimensionless
-A = np.pi*0.5**2 # cross section, m^2
-EA = 75 # angle of elevation, in deg
+mass_launch = 6200  # kg, launch mass
+mass_empty_rocket = 1600  # kg, mass of war head + empty rocket
+mass_warhead = 800  # kg, mass of war head
+isp = 237  # s, SRB general number todo： ？
+v_e = isp * g_const  # m/s
+TTW_const = 1.5  # thrust to weight
+thrust_force = mass_launch * g_const * TTW_const  # thrust, in N
+drag_coefficient = 2e-1  # drag coefficient, dimensionless
+A = np.pi * 0.5 ** 2  # cross section, m^2
+ELEVATION_ANGLE = 75  # angle of elevation, in deg
 
 # Atmospheric constants
-T0 = 273.15 + 15 # K, surface temperature
-p0 = 1013e2 # Pa, surface pressure
-R  = 8.314 # J/K/mol, ideal gas constant
-L = 0.0065 # K/m, temperature lapse rate
-MW = 0.029 # kg/mol, molecular weight of the air
+surface_temperature = 273.15 + 15  # K, surface temperature
+surface_pressure = 1013e2  # Pa, surface pressure
+gas_const = 8.314  # J/K/mol, ideal gas constant
+temperature_lapse_rate_const = 0.0065  # K/m, temperature lapse rate
+molecular_weight_const = 0.029  # kg/mol, molecular weight of the air
 
-#%%
+# %%
 
 # Simulation properties
-tmax = 3.6e3 # Simulation time in second, set to 60 min for maximum
+max_t = 3.6e3  # Simulation time in second, set to 60 min for maximum
 
 # Visualization properties
-Box_size = 1.2e7 # Size of the plot
-frames = int(2100) # Output frames
-Tracing = True # Viewing the sail with tracing mode.
-SAVE_VIDEO = True  # Whether you want to save the video
+plot_size = 1.2e7  # Size of the plot
+num_frames = int(2100)  # Output frames
+Tracing = True  # Viewing the sail with tracing mode.
+SAVE_VIDEO = False  # Whether you want to save the video
 
-#%%
+
+# %%
 def initial_condition():
     def launch_parameter():
-        return [0, R_E, 0, 100, m_0] # x, y, vx, vy, m
+        return [0, RADIUS_EARTH, 0, 100, mass_launch]  # x, y, vx, vy, m
+        # 可以看出，原点是地心
+    
     return launch_parameter()
-y_0 = initial_condition()
 
-def rotate(vector, theta): # in degree
-    theta = theta/180*np.pi
+
+initial_states = initial_condition()
+
+
+def rotate(vector, theta):  # in degree
+    theta = theta / 180 * np.pi
     rotation_matrix = np.array([
         [np.cos(theta), -np.sin(theta)],
         [np.sin(theta), np.cos(theta)]
     ])
     return np.matmul(rotation_matrix, vector)
-    
+
+
 def air_density(h):
     # Here we are modeling air density by extrapolating Troposphere condition
-    if h < 4.4e4: # 44 km
-        rho = p0*MW/R/T0*(1-L*h/T0)**(g*MW/R/L-1)
+    if h < 4.4e4:  # 44 km
+        rho = surface_pressure * molecular_weight_const / gas_const / surface_temperature * (
+                1 - temperature_lapse_rate_const * h / surface_temperature) ** (
+                      g_const * molecular_weight_const / gas_const / temperature_lapse_rate_const - 1)
     else:
         rho = 0
     return rho
 
-#%% Alltitude control
-# This is the tricky part, lots of freedom included.
-# Here we use: 
-#    1. When the missile is still in a dense atmosphere, we let it accelerate
-#       in a direction similar to rhat (slight deviation from going straight up)
-#    2. When above 10 km, we let the acceleration align with the velocity vector. 
-def Decide_Pointing(t, x, y, vx, vy):
-    r = np.array([x, y])
+
+"""%% Altitude control
+This is the tricky part, lots of freedom included.
+Here we use:
+   1. When the missile is still in a dense atmosphere, we let it accelerate
+      in a direction similar to rhat (slight deviation from going straight up)
+   2. When above 10 km, we let the acceleration align with the velocity vector. """
+
+
+def calc_pointing(t, x, y, vx, vy):
+    p = np.array([x, y])
     v = np.array([vx, vy])
-    rhat = r/np.linalg.norm(r)
+    p_pointing = p / np.linalg.norm(p)  # 原点（地心）到 p 的方向向量
     if vx == 0 and vy == 0:
-        vhat = rhat
+        v_pointing = p_pointing
     else:
-        vhat = v/np.linalg.norm(v)
-    Acc = False
-    if np.linalg.norm(r) > R_E + 1e4: # above 10 km, align with velocity
-        phat = vhat
+        v_pointing = v / np.linalg.norm(v)
+    
+    if np.linalg.norm(
+            p) > RADIUS_EARTH + 1e4:  # above 10 km, align with velocity
+        phat = v_pointing
     else:
-        if np.dot(r, v) > 0:
-            phat = rotate(rhat, EA-90)
-        else: 
-            phat = vhat
+        if np.dot(p, v) > 0:  # 向下飞
+            phat = rotate(p_pointing, ELEVATION_ANGLE - 90)
+        else:  # 向上飞
+            phat = v_pointing
     return phat
 
-#%% Here are the function for ivp solver
-def impact(t, y): return np.sqrt(y[0]**2 + y[1]**2) - R_E
-impact.terminal = True
-impact.direction = -1
 
-def function(t, y):
+# %% Here are the function for ivp solver
+def impact_event(t, y):
+    return np.sqrt(y[0] ** 2 + y[1] ** 2) - RADIUS_EARTH
+
+
+impact_event.terminal = True
+impact_event.direction = -1
+
+
+def state_function(t, states):
+    """
+    
+    :param t:
+    :param states: np.array([x, y, vx, vy, 剩余总重 m])
+    :return: d_states: np.array([x, y, vx, vy, dm])
+    """
     separation = False
-    r_vec = y[:2]
-    r = np.linalg.norm(r_vec)
-    v_vec = y[2:4]
-    v = np.linalg.norm(v_vec)
-    phat = Decide_Pointing(t, y[0], y[1], y[2], y[3])
+    p_vec = states[:2]
+    radius = np.linalg.norm(p_vec)  # 到地心距离
+    v_vec = states[2:4]
+    v_norm = np.linalg.norm(v_vec)
+    phat = calc_pointing(t, states[0], states[1], states[2], states[3])
     dxdt = v_vec[0]
     dydt = v_vec[1]
     
     # This is for thrust, only exist when there are still enough fuel
-    if y[4] > m_f:
-        a_rocket = F/y[4]*phat
-        dmdt = -F/v_e
+    if states[4] > mass_empty_rocket:
+        acc_thrust = thrust_force / states[4] * phat
+        dmdt = -thrust_force / v_e
     else:
         separation = True
-        a_rocket = 0
+        acc_thrust = 0
         dmdt = 0
+    
     # Gravity
-    a_g = -G*M_E/r**3*r_vec
+    acc_grav = -GRAV_CONST * MASS_EARTH / radius ** 3 * p_vec
+    
     # Air drag
-    if separation: # decent
-        a_A = -0.5*air_density(r-R_E)*v*C_D*(0.5*A)/m_wh*v_vec
-    else: # acent
-        a_A = -0.5*air_density(r-R_E)*v*C_D*A/y[4]*v_vec
+    if separation:  # descending
+        acc_air = -0.5 * air_density(
+            radius - RADIUS_EARTH) * v_norm * drag_coefficient * (
+                          0.5 * A) / mass_warhead * v_vec
+    else:  # ascending
+        acc_air = -0.5 * air_density(
+            radius - RADIUS_EARTH) * v_norm * drag_coefficient * A / \
+                  states[4] * v_vec
     
     # Centrifugal force
-    a_cen = omega**2*r_vec
+    acc_centrifugal = OMEGA_EARTH ** 2 * p_vec  # 离心加速度
     
     # Coriolis force
-    a_cor = 2*omega*np.array([v_vec[1], -v_vec[0]])
+    acc_coriolis = 2 * OMEGA_EARTH * np.array([v_vec[1], -v_vec[0]])
     
     # Total acceleration
-    a = a_rocket + a_g + a_A + a_cen + a_cor
-    dvxdt = a[0]
-    dvydt = a[1]
-    return np.array([dxdt, dydt, dvxdt, dvydt, dmdt])
-#%%
+    acc = acc_thrust + acc_grav + acc_air + acc_centrifugal + acc_coriolis
+    dvxdt = acc[0]
+    dvydt = acc[1]
+    
+    d_states = np.array([dxdt, dydt, dvxdt, dvydt, dmdt])
+    return d_states
+
+
+# %%
 # Solving the orbit
-sol = solve_ivp(fun=function,
-                t_span=[0, tmax],
-                y0=y_0,
-                t_eval=np.linspace(0,tmax,frames),
-                events=impact,
+sol = solve_ivp(fun=state_function,
+                t_span=[0, max_t],
+                y0=initial_states,
+                t_eval=np.linspace(0, max_t, num_frames),
+                events=impact_event,
                 method='DOP853')
 
 t = sol.t
 Data = sol.y
-r = np.sqrt(Data[0,:]**2 + Data[1,:]**2)
-x = Data[0,:]
-y = Data[1,:]
-vx = Data[2,:]
-vy = Data[3,:]
-m  = Data[4,:]
+r = np.sqrt(Data[0, :] ** 2 + Data[1, :] ** 2)
+x = Data[0, :]
+y = Data[1, :]
+vx = Data[2, :]
+vy = Data[3, :]
+m = Data[4, :]
 
-#%%
+# %%
 # Visualization Setup
 COLOR = '#303030'
 LineColor = 'silver'
-fig = plt.figure(figsize = (8, 4.5), facecolor=COLOR)
+fig = plt.figure(figsize=(8, 4.5), facecolor=COLOR)
 gs = GridSpec(2, 4, figure=fig)
 
 # Picture
@@ -172,32 +206,34 @@ ax.set_facecolor('#202020')
 ax.xaxis.set_visible(False)
 ax.yaxis.set_visible(False)
 ax.spines['bottom'].set_color(COLOR)
-ax.spines['top'].set_color(COLOR) 
+ax.spines['top'].set_color(COLOR)
 ax.spines['right'].set_color(COLOR)
 ax.spines['left'].set_color(COLOR)
 
-earth = plt.Circle((0, 0), R_E, color='cyan')
+earth = plt.Circle((0, 0), RADIUS_EARTH, color='cyan')
 ax.add_patch(earth)
 ax.set_aspect('equal', 'box')
 
 line, = ax.plot(x[0], y[0], color='silver', linestyle='-', linewidth=1)
-dot, = ax.plot([], [], color='silver', marker='o', markersize=1, markeredgecolor='w', linestyle='')
+dot, = ax.plot([], [], color='silver', marker='o', markersize=1,
+               markeredgecolor='w', linestyle='')
 
-ax.set_xlim([-Box_size,Box_size])
-ax.set_ylim([-Box_size,Box_size])
-#%%
+ax.set_xlim([-plot_size, plot_size])
+ax.set_ylim([-plot_size, plot_size])
+# %%
 # Velocity Plot
 ax1 = fig.add_subplot(gs[0, 2:])
 ax1.set_facecolor(COLOR)
-velline, = ax1.plot(t[0], np.sqrt(vx[0]**2+vy[0]**2), color='silver')
+velline, = ax1.plot(t[0], np.sqrt(vx[0] ** 2 + vy[0] ** 2), color='silver')
 ax1.spines['bottom'].set_color(LineColor)
-ax1.spines['top'].set_color(LineColor) 
+ax1.spines['top'].set_color(LineColor)
 ax1.spines['right'].set_color(LineColor)
 ax1.spines['left'].set_color(LineColor)
-ax1.set_xlim([0,t[-1]])
-ax1.set_ylim([0,np.max(np.sqrt(vx**2+vy**2))*1.2])
-ax1.tick_params(labelcolor=LineColor, labelsize='medium', width=3, colors=LineColor)
-ax1.ticklabel_format(axis='y', style='sci', useMathText=True, scilimits=(4,5))
+ax1.set_xlim([0, t[-1]])
+ax1.set_ylim([0, np.max(np.sqrt(vx ** 2 + vy ** 2)) * 1.2])
+ax1.tick_params(labelcolor=LineColor, labelsize='medium', width=3,
+                colors=LineColor)
+ax1.ticklabel_format(axis='y', style='sci', useMathText=True, scilimits=(4, 5))
 ax1.set_xlabel('Time (s)')
 ax1.set_ylabel('Velocity (m/s)')
 ax1.xaxis.label.set_color(LineColor)
@@ -206,57 +242,64 @@ ax1.yaxis.label.set_color(LineColor)
 # height Plot
 ax2 = fig.add_subplot(gs[1, 2:])
 ax2.set_facecolor(COLOR)
-r = np.sqrt(x**2 + y**2)
-h = r-R_E
+r = np.sqrt(x ** 2 + y ** 2)
+h = r - RADIUS_EARTH
 heightline, = ax2.plot(t[0], h[0], color='silver')
 ax2.spines['bottom'].set_color(LineColor)
-ax2.spines['top'].set_color(LineColor) 
+ax2.spines['top'].set_color(LineColor)
 ax2.spines['right'].set_color(LineColor)
 ax2.spines['left'].set_color(LineColor)
 ax2.set_xlim([0, t[-1]])
-ax2.set_ylim([0, np.max(h)*1.2])
-ax2.tick_params(labelcolor=LineColor, labelsize='medium', width=3, colors=LineColor)
-ax2.ticklabel_format(style='sci', useMathText=True, scilimits=(4,5))
+ax2.set_ylim([0, np.max(h) * 1.2])
+ax2.tick_params(labelcolor=LineColor, labelsize='medium', width=3,
+                colors=LineColor)
+ax2.ticklabel_format(style='sci', useMathText=True, scilimits=(4, 5))
 ax2.set_xlabel('Time (s)')
 ax2.set_ylabel('Height (m)')
 ax2.xaxis.label.set_color(LineColor)
 ax2.yaxis.label.set_color(LineColor)
 
-#%% Suptitle
+# %% Suptitle
 
-Range = R_E*np.arccos(np.dot([x[0], y[0]], [x[-1],y[-1]])/R_E**2)/1000 # in km
-v_f = np.sqrt(vx[-2]**2+vy[-2]**2)/340 # in Mach
-fig.suptitle('Launch angle {:.0f} degree, Range: {:.0f} km, impact velocity M{:.1f}'.format(EA, Range, v_f), color='silver')
+Range = RADIUS_EARTH * np.arccos(
+    np.dot([x[0], y[0]], [x[-1], y[-1]]) / RADIUS_EARTH ** 2) / 1000  # in km
+v_f = np.sqrt(vx[-2] ** 2 + vy[-2] ** 2) / 340  # in Mach
+fig.suptitle(
+    'Launch angle {:.0f} degree, Range: {:.0f} km, impact velocity M{:.1f}'.format(
+        ELEVATION_ANGLE, Range, v_f), color='silver')
 
 plt.tight_layout()
-#%%
-ms2AUyr = 86400*365/1.5e11
+# %%
+ms2AUyr = 86400 * 365 / 1.5e11
+
+
 def update(i):
     dot.set_data(x[i], y[i])
     line.set_data(x[:i], y[:i])
-    velline.set_data(t[:i], np.sqrt(vx[:i]**2+vy[:i]**2))
+    velline.set_data(t[:i], np.sqrt(vx[:i] ** 2 + vy[:i] ** 2))
     heightline.set_data(t[:i], h[:i])
     if Tracing:
-        #ax.set_xlim([-1.5*r,1.5*r])
-        #ax.set_ylim([-1.5*r,1.5*r])
-        ax.set_xlim([x[i]-1e5,x[i]+1e5])
-        ax.set_ylim([y[i]-1e5,y[i]+1e5])
-        #ax.set_xlim([np.min(x)-1e5,np.max(x)+1e5])
-        #ax.set_ylim([np.min(y)-1e5,np.max(y)+1e5])
+        # ax.set_xlim([-1.5*r,1.5*r])
+        # ax.set_ylim([-1.5*r,1.5*r])
+        ax.set_xlim([x[i] - 1e5, x[i] + 1e5])
+        ax.set_ylim([y[i] - 1e5, y[i] + 1e5])
+        # ax.set_xlim([np.min(x)-1e5,np.max(x)+1e5])
+        # ax.set_ylim([np.min(y)-1e5,np.max(y)+1e5])
     O1 = ax.add_patch(earth)
     if SAVE_VIDEO:
         print(i)
         fig.savefig('./images/{:04d}.jpg'.format(i), dpi=300)
     return [dot, line, velline, heightline, O1]
 
+
 if SAVE_VIDEO:
-    for i in range(frames):
+    for i in range(len(t)):  # 考虑到提前结束，不能是num_frames，而是t长度
         update(i)
 else:
-    ani = FuncAnimation(fig=fig, 
+    ani = FuncAnimation(fig=fig,
                         func=update,
-                        frames=frames, 
-                        interval=10000/frames, 
-                        blit=True, 
+                        frames=len(t),
+                        interval=10000 / num_frames,
+                        blit=True,
                         repeat=False)
     plt.show()
